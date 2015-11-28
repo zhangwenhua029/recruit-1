5f1719caf966f0caa87f649c7f158547b39a1af3
Victoria <bulasv@gmail.com>
2015-11-28 01:46:16 +0100

    getting course from Yahoo parsing AJAX data 1) make a calendar 2) how to get data in Json format from API

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Write your jQuery code on line 3!
-$(document).ready(function() {
-    var $target = $('#trash');
-    $target.fadeOut('fast');
+$(document).ready(function () {
+    $(function () {
+        $("#datepicker").datepicker({
+            firstDay: 1,
+            onSelect: function (dateText) {
+                $('#dateValue').val(dateText);
+            }
+        });
+
+    });
+
+
+    $('#selectCourse').click(function () {
+        $.ajax({
+                url: "https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDUAH,EURUAH%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=",
+                cache: false
+            })
+            .done(function (html) {
+                $('#datka tr').remove();
+                $.each(html.query.results.rate, function () {
+                    $("#datka").append("<tr></tr>");
+                    $('#datka tr:last').after('<tr><td>' + this.Time + '</td> +  <td>' + this.Name + '</td>+ <td>' + this.Rate + '</td>+</tr>');
+                });
+            });
+    });
 });
+
+
